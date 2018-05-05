9f9dd093d258e967c04c51ed22176ab0728bf730
UPC-PC\UPC <878437915@qq.com>
2018-05-05 13:20:24 +0800

    modify texture id

--- BerlinRenderer/Model.h
+++ BerlinRenderer/Model.h
@@ -189,7 +189,7 @@ GLint TextureFromFile(const char* path, string directory)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindTexture(GL_TEXTURE_2D, textureID);
 	SOIL_free_image_data(image);
 	return textureID;
 }
