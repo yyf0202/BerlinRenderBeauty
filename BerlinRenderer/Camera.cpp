59ee5597763e634f4bfb8480340998b38b495a3e
UPC-PC\UPC <878437915@qq.com>
2018-05-19 15:40:04 +0800

    implementaion Camera

--- BerlinRenderer/Camera.cpp
+++ BerlinRenderer/Camera.cpp
@@ -15,4 +15,27 @@ namespace BRE {
 	Camera::~Camera()
 	{
 	}
+
+	void Camera::SetProjectionParams(float fov, float aspect, float nearZ, float farZ) {
+		this->fov = fov;
+		this->aspect = aspect;
+		this->near_z_ = nearZ;
+		this->far_z_ = farZ;
+
+
+	}
+	void Camera::SetViewParams(glm::vec3 const& cameraPos, glm::vec3 const& lookatPos) {
+		this->pos_ = cameraPos;
+		this->target_pos_ = lookatPos;
+		this->forward_ = glm::normalize(lookatPos - cameraPos);
+	}
+	void SetViewParams(glm::vec3 const& cameraPos, glm::vec3 const& lookatPos, glm::vec3 const& upVec);
+
+	void SetCameraPos(glm::vec3 const& cameraPos);
+	void LookAt(glm::vec3 const& lookatPos);
+
+	float Fov();
+	float Aspect();
+	float NearZ();
+	float FarZ();
 }
--- BerlinRenderer/Camera.h
+++ BerlinRenderer/Camera.h
@@ -13,5 +13,34 @@ namespace BRE {
 	public:
 		Camera();
 		~Camera();
+		void SetProjectionParams(float fov, float aspect, float nearZ, float farZ);
+		void SetViewParams(glm::vec3 const& cameraPos, glm::vec3 const& lookatPos);
+		void SetViewParams(glm::vec3 const& cameraPos, glm::vec3 const& lookatPos, glm::vec3 const& upVec);
+
+		void SetCameraPos(glm::vec3 const& cameraPos);
+		void LookAt(glm::vec3 const& lookatPos);
+
+		float Fov();
+		float Aspect();
+		float NearZ();
+		float FarZ();
+		glm::vec3 const& cameraPos();
+		glm::vec3 const& Forward();
+		glm::vec3 const& Right();
+		glm::vec3 const& Up();
+
+	private:
+		glm::vec3 pos_;
+		glm::vec3 target_pos_;
+		glm::vec3 forward_;
+		glm::vec3 right_;
+		glm::vec3 up_;
+		glm::vec3 up_vec_;
+		glm::mat4 view_matrix_;
+		glm::mat4 project_matrix_;
+		float aspect;
+		float fov;
+		float near_z_;
+		float far_z_;
 	};
 }
