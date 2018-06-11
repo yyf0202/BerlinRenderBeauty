d3e3811f6377d40c409aeabc530fb57584bd418d
suchy <191986857@qq.com>
2018-06-11 23:30:29 +0800

    change res path

--- BerlinRenderer/Tester/TextureRenderTester.h
+++ BerlinRenderer/Tester/TextureRenderTester.h
@@ -4,10 +4,31 @@ namespace tester
 {
 	class TextureRenderTester
 	{
+	private:
+		class _TestApp : public App
+		{
+		public:
+			_TestApp() : App(string_t("TextureRenderTester")) {}
+			virtual void Startup() override
+			{
+				auto obj = new SceneObject();
+				auto textRender = new TextureRender();
+				obj->SetRender(textRender);
+				auto material = new Material();
+				material->SetShaderPath("./Resources/Shaders/diffuse");
+				obj->GetRenderer()->SetMaterial(material);
+				auto text = new Texture2D();
+				text->Load("./Resources/Textures/test.png");
+				textRender->SetTexture(text);
+
+				Context::GetInstance().SceneManagerInstance().AddSceneObject(shared_ptr_t<SceneObject>(obj));
+			}
+		};
 	public:
 		static bool Run()
 		{
-
+			auto app = new _TestApp();
+			app->Run();
 
 
 
